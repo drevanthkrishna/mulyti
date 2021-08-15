0562357878848a3be85da7a78f5360faacadadfa
drevanthkrishna <78145539+drevanthkrishna@users.noreply.github.com>
2021-08-15 12:24:48 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,17 @@
+var canvas;
+var backgroundImage;
+var gameState=0;
+var playerCount;
+var form,player,game;
+
+function setup(){
+canvas=createCanvas(400,400);
+database=firebase.database();
+
+}
+function draw (){
+
+
+
+  
+}
